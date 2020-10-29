b289b126266d6f5f230d3d8eb9705cc9790969e2
imprevo <zhenek42@gmail.com>
2020-10-30 00:30:53 +0700

    add second player controls

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,21 @@ const clamp = (val: number, min: number, max: number) =>
 
 const WIDTH = 800;
 const HEIGHT = 600;
+const BALL_SPEED = 200;
 
 type GameObjectWithPhysics = Phaser.GameObjects.Shape & {
   body: Phaser.Physics.Arcade.Body;
 };
 
+type PlayerKeys = Record<'up' | 'down' | 'start', Phaser.Input.Keyboard.Key>;
+
 export class PongScene extends Phaser.Scene {
   player1: GameObjectWithPhysics;
+  player1Keys: PlayerKeys;
+
   player2: GameObjectWithPhysics;
+  player2Keys: PlayerKeys;
+
   ball: GameObjectWithPhysics;
 
   isStart = false;
@@ -28,22 +35,32 @@ export class PongScene extends Phaser.Scene {
 
   create() {
     this.player1 = this.add.rectangle(
-      40,
+      20,
       HEIGHT / 2,
       20,
       100,
       0xffffff
     ) as GameObjectWithPhysics;
     this.physics.add.existing(this.player1);
+    this.player1Keys = {
+      up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      start: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+    };
 
     this.player2 = this.add.rectangle(
-      WIDTH - 40,
+      WIDTH - 20,
       HEIGHT / 2,
       20,
       100,
       0xffffff
     ) as GameObjectWithPhysics;
     this.physics.add.existing(this.player2);
+    this.player2Keys = {
+      up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
+      down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN),
+      start: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER),
+    };
 
     this.ball = this.add.circle(
       -100,
@@ -55,17 +72,24 @@ export class PongScene extends Phaser.Scene {
   }
 
   update() {
-    const cursorKeys = this.input.keyboard.createCursorKeys();
     const player1Body = this.player1.body;
     const player2Body = this.player2.body;
     const ballBody = this.ball.body;
 
-    if (cursorKeys.up.isDown) {
+    if (this.player1Keys.up.isDown) {
       player1Body.setVelocityY(-500);
-    } else if (cursorKeys.down.isDown) {
+    } else if (this.player1Keys.down.isDown) {
       player1Body.setVelocityY(500);
     } else {
       player1Body.setVelocityY(0);
+    }
+
+    if (this.player2Keys.up.isDown) {
+      player2Body.setVelocityY(-500);
+    } else if (this.player2Keys.down.isDown) {
+      player2Body.setVelocityY(500);
+    } else {
+      player2Body.setVelocityY(0);
     }
 
     player1Body.y = clamp(player1Body.y, 0, HEIGHT - player1Body.height);
@@ -79,19 +103,22 @@ export class PongScene extends Phaser.Scene {
         this.isStart = false;
         this.side = 2;
       } else {
-        if (
-          this.physics.overlap(this.ball, this.player1) ||
-          this.physics.overlap(this.ball, this.player2)
-        ) {
-          ballBody.setVelocityX(ballBody.velocity.x * -1);
+        if (this.physics.overlap(this.ball, this.player1)) {
+          ballBody.setVelocityX(BALL_SPEED);
+        }
+        if (this.physics.overlap(this.ball, this.player2)) {
+          ballBody.setVelocityX(-BALL_SPEED);
         }
         if (ballBody.y > HEIGHT - ballBody.height || ballBody.y < 0) {
           ballBody.setVelocityY(ballBody.velocity.y * -1);
         }
       }
-    } else if (cursorKeys.space.isDown) {
+    } else if (this.side == 1 && this.player1Keys.start.isDown) {
       this.isStart = true;
-      ballBody.setVelocity(200, 200);
+      ballBody.setVelocity(BALL_SPEED, BALL_SPEED);
+    } else if (this.side == 2 && this.player2Keys.start.isDown) {
+      this.isStart = true;
+      ballBody.setVelocity(-BALL_SPEED, BALL_SPEED);
     } else {
       if (this.side == 1) {
         ballBody.x = player1Body.x + player1Body.width;
