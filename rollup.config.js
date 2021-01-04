ccfc477f64624a5c4721d581fe4e77a334bb88bd
imprevo <zhenek42@gmail.com>
2021-01-04 19:55:32 +0700

    fix rollup compilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import html from '@rollup/plugin-html';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import serve from 'rollup-plugin-serve';
